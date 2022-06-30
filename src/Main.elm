32edc22ad28041bfd164330036fc71a9d0fb695b
Berto <bertocode@gmail.com>
2022-06-30 20:51:25 +0200

    Feat: Render grid and enables basic actions
    After this commit, you can see the grid of the GameOfLife and toggle the
    cells between alive and not alive clicking on the cell.
    
    It also enables the actions for the button, although the ticking effects
    of time are not enabled yet.

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,38 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, div)
+import Html exposing (Html, button, div, node, text)
+import Html.Attributes as Attr exposing (class, classList, href, rel)
+import Html.Events exposing (onClick)
+import Set exposing (Set)
+
+
+gridSize : ( Int, Int )
+gridSize =
+    ( 30, 30 )
 
 
 type Msg
-    = NoOp
+    = Play
+    | Pause
+    | ToggleGridElement ( Int, Int )
+    | Reset
 
 
 type alias Model =
-    {}
+    { gameState : GameState
+    , activeCells : Set ( Int, Int )
+    }
+
+
+type GameState
+    = Playing
+    | Paused
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { gameState = Paused, activeCells = Set.empty }, Cmd.none )
 
 
 main : Program () Model Msg
@@ -29,9 +47,56 @@ main =
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Play ->
+            ( { model | gameState = Playing }, Cmd.none )
+
+        Pause ->
+            ( { model | gameState = Paused }, Cmd.none )
+
+        ToggleGridElement ( x, y ) ->
+            if Set.member ( x, y ) model.activeCells then
+                ( { model | activeCells = Set.remove ( x, y ) model.activeCells }, Cmd.none )
+
+            else
+                ( { model | activeCells = Set.insert ( x, y ) model.activeCells }, Cmd.none )
+
+        Reset ->
+            ( { model | activeCells = Set.empty }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    div [] []
+    div []
+        [ case model.gameState of
+            Playing ->
+                button [ onClick Pause ] [ text "Pause" ]
+
+            Paused ->
+                button [ onClick Play ] [ text "Play" ]
+        , div [ class "game-of-life" ]
+            [ renderGrid model ]
+        ]
+
+
+renderGrid : Model -> Html Msg
+renderGrid model =
+    div [ class "row" ] <|
+        (List.range -(Tuple.first gridSize) (Tuple.first gridSize)
+            |> List.map
+                (\x ->
+                    div [ class "column" ] <|
+                        (List.range -(Tuple.second gridSize) (Tuple.second gridSize)
+                            |> List.map
+                                (\y ->
+                                    div
+                                        [ classList [ ( "item", True ), ( "alive", Set.member ( x, y ) model.activeCells ) ]
+                                        , Attr.attribute "y" <| String.fromInt y
+                                        , Attr.attribute "x" <| String.fromInt x
+                                        , onClick <| ToggleGridElement ( x, y )
+                                        ]
+                                        []
+                                )
+                        )
+                )
+        )
